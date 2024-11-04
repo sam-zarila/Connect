d27d2b99ef24abe062970ced61d3767df96eb7d3
sam zarila <zarilatroxx99@gmail.com>
2024-11-05 00:23:48 +0100

    working

--- src/farmtools/farmtools.controller.ts
+++ src/farmtools/farmtools.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { FarmtoolsService } from './farmtools.service';
 import { farmEntity } from 'src/Entities/FarmEntity';
 import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { farmToolDto } from 'src/DTOs/createFarmTool.Dto';
 
 
 
@@ -19,7 +20,16 @@ export class FarmtoolsController {
 
     return this.farmToolsService.createFarmTools(farmEntity)
   }
+@Get()
+@ApiOperation({summary:'retrieving farm tools'})
+findFarmTools():Promise<farmToolDto[]>{
 
+  return this.farmToolsService.findFarmTools()
+
+}
+
+@Delete()
+@ApiOperation({summary:'deleting farm tools'})
 
  
 }
