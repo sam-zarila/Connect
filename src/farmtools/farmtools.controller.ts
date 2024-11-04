55e38f5022766b6b8d7072c34d9125053a7c5fad
sam zarila <zarilatroxx99@gmail.com>
2024-11-05 00:17:08 +0100

    working

--- src/farmtools/farmtools.controller.ts
+++ src/farmtools/farmtools.controller.ts
@@ -1,9 +1,25 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { FarmtoolsService } from './farmtools.service';
 import { farmEntity } from 'src/Entities/FarmEntity';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 
+
+@ApiTags('Farm Tools')
 @Controller('farmtools')
 export class FarmtoolsController {
+
+  constructor(private readonly farmToolsService: FarmtoolsService) {}
+
+  @Post()
+  @ApiOperation({summary:'creating farm tools'})
+  @ApiBody({type: farmEntity})
+
+  createFarmTools(@Body() farmEntity:farmEntity){
+
+    return this.farmToolsService.createFarmTools(farmEntity)
+  }
+
+
  
 }
