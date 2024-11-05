1f80383785d2abb40cdead40f583b3c34cc1ac53
sam zarila <zarilatroxx99@gmail.com>
2024-11-05 19:33:39 +0100

    markets

--- src/chemical/chemical.service.ts
+++ src/chemical/chemical.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { chemicalsEntity } from './entities/chemical.entity';
+import { Repository } from 'typeorm';
+import { chemicalDTO } from './dto/create-chemical.dto';
 
 
 @Injectable()
 export class ChemicalService {
+
+  constructor(
+    @InjectRepository(chemicalsEntity) private chemicalsRepository:Repository<chemicalsEntity>
+  ){}
+
+  createChemicaSales( chemical:chemicalsEntity){
+    
+   return this.chemicalsRepository.save(chemical);
+
+
+  }
+
+  getChemicaSales() :Promise<chemicalDTO[]>{
   
+     return this.chemicalsRepository.find()
+
+  }
+ async deleteChemicalSales( id:number) :Promise<void>{
+  
+  await this.chemicalsRepository.delete(id)
+
+ }
+
 
 }
