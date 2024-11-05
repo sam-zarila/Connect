b3693569760cb4f1f892196cec6bbd43b9c4a6c3
sam zarila <zarilatroxx99@gmail.com>
2024-11-05 19:13:20 +0100

    markets

--- src/chemical/entities/chemical.entity.ts
+++ src/chemical/entities/chemical.entity.ts
@@ -1,1 +1,31 @@
-export class Chemical {}
+
+import { ApiProperty } from "@nestjs/swagger";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+
+export class chemicalsEntity{
+    @PrimaryGeneratedColumn()
+    @ApiProperty()
+     id:number
+
+     @Column()
+     @ApiProperty()
+     image:string
+
+     @Column()
+     @ApiProperty()
+
+     name:string
+
+     @Column()
+     @ApiProperty()
+     description:string
+
+     @Column()
+     @ApiProperty()
+
+     price:string
+
+
+}
